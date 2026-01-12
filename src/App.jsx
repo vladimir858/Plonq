a1c0309ee35c60f7f163526aa4578129e5a8e79f
vladimir858 <standardwebsites@gmail.com>
2026-01-12 12:08:30 +0300

    Create Heder-nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Header from './components/Header';
 
 function App() {
   return (
-    <Header />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
-
-
-
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,32 @@
+import { Link } from 'react-router-dom';
+
 export default function Header() {
   return (
     <header className="bg-white top-0">
-      <div className="absolute inset-0 bg-[url('../../public/500.png')] bg-cover bg-center">
-      <div className=" flex justify-between items-center bg-white relative  z-50 pt-[22px]  pb-[22px] pl-[256px] ">
-
-        <div className=" flex">
-        <div className="mr-[632px]" >
-      <img src="../../public/Logo.svg" />
+      <div className="absolute inset-0 bg-[url('/500.png')] bg-cover bg-center">
+        <div className="flex justify-between items-center bg-white relative z-50 pt-[22px] pb-[22px] pl-[256px]">
+          <div className="flex">
+            <div className="mr-[632px]">
+              <img src="/Logo.svg" alt="Логотип" />
+            </div>
+            <nav className="">
+              {/* Замени <a> на <Link> и href на to */}
+              <Link to="/" className="ml-[124px] hover:text-[#c8bb93]  transition ">
+                Главная
+              </Link>
+              <Link to="/about" className="ml-[124px] hover:text-[#c8bb93] transition  ">
+                Обо мне
+              </Link>
+              <Link to="/projects" className="ml-[124px] hover:text-[#c8bb93] transition " >
+                Проекты
+              </Link>
+              <Link to="/contact" className="ml-[124px] hover:text-[#c8bb93] transition  ">
+                Контакты
+              </Link>
+            </nav>
+          </div>
         </div>
-        <nav className="">
-          <a href="#" className="ml-[124px]">
-            Главная
-          </a>
-          <a href="#" className="ml-[124px]">
-            Обо мне
-          </a>
-          <a href="#" className="ml-[124px]">
-            Проекты
-          </a>
-          <a href="#" className="ml-[124px]">
-            Контакты
-          </a>
-        </nav>
-             </div>
-
-
       </div>
-           </div>
     </header>
   );
 }
