c8c0ca3dd7d88806f4ce98b3abebc936811066f0
vladimir858 <standardwebsites@gmail.com>
2026-01-21 14:57:34 +0300

    readme

--- src/components/Header-nav.jsx
+++ src/components/Header-nav.jsx
@@ -2,14 +2,15 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className="bg-white top-0">
+    <header className="header">
         <div className="flex justify-between items-center bg-white relative z-50 pt-[22px] pb-[22px] pl-[256px]">
           <div className="flex">
             <div className="mr-[632px]">
               <img src="/Logo.svg" alt="Логотип" />
             </div>
-            <nav className="">
-              <Link to="/" className="ml-[124px] hover:text-[#c8bb93]  font-normal transition ">
+            <nav className="header-nav">
+              <Link to="/" className="ml-[124px] hover:text-[#c8bb93] 
+               font-normal transition  header-nav-link">
                 Продукты
               </Link>
               <Link to="/about" className="ml-[124px] hover:text-[#c8bb93] transition  font-normal ">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import ButtonDetail from '../components/button-detail';
 export default function Home() {
   return (
     <div className="">
-    <div className="min-h-screen bg-[url('/500.png')] bg-cover bg-center pt-[100px]">
+    {/* <div className="min-h-screen bg-[url('/500.png')] bg-cover bg-center pt-[100px]">
      
       <svg width="55" height="17" viewBox="0 0 55 17" fill="none" xmlns="http://www.w3.org/2000/svg" className='ml-[440px] mt-[200px]'>
   <path d="M0 16.8798V0.0480898H1.67907L11.6854 13.5375V0.0480898H13.4326V16.8798H12.0031L1.74715 3.07779V16.8798H0ZM17.9261 16.8798V0.0480898H29.4073V1.77935H19.7186V7.52617H28.3863V9.25743H19.7186V15.1485H29.5208V16.8798H17.9261ZM37.5739 17L31.9015 0.0480898H33.8527L38.3908 14.0665L42.7473 0H44.2222L48.5787 14.0665L53.1167 0.0480898H55L49.3275 17H47.8072L43.4507 3.34229L39.0715 17H37.5739Z" fill="#92E7E4" />
@@ -121,7 +121,7 @@ export default function Home() {
       </div>
     </div>
 
-   </div>
+   </div> */}
 
 <div className="flex  
 justify-center mt-[140px]">
