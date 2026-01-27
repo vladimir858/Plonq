1bfda8828b6fd46dff15186fe08a19b1992d12b7
vladimir858 <standardwebsites@gmail.com>
2026-01-27 21:18:51 +0300

    css fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
 <footer className="    footer ">
+  <div className="container">
+
+
   <a href="#" className=" ">
     <img src="/public/Plonq_logo.svg"   />
   </a>
@@ -89,7 +92,7 @@ export default function Footer() {
   <div className="text-normal  warning     ">
 ПРОДАЖА НЕСОВЕРШЕННОЛЕТНИМ ЗАПРЕЩЕНА. Сайт используется исключительно в целях информирования действующих потребителей продукции PLONQ Защищено выпущенными и/или находящимися на одобрении международными патентами..
   </div>
-
+  </div>
 </footer>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <div className="">
     <div className=" header-home">
+      <div className="container">
+
+    
       <svg width="55" height="17" viewBox="0 0 55 17" fill="none" xmlns="http://www.w3.org/2000/svg" 
       className='strelka-home'>
   <path d="M0 16.8798V0.0480898H1.67907L11.6854 13.5375V0.0480898H13.4326V16.8798H12.0031L1.74715 3.07779V16.8798H0ZM17.9261 16.8798V0.0480898H29.4073V1.77935H19.7186V7.52617H28.3863V9.25743H19.7186V15.1485H29.5208V16.8798H17.9261ZM37.5739 17L31.9015 0.0480898H33.8527L38.3908 14.0665L42.7473 0H44.2222L48.5787 14.0665L53.1167 0.0480898H55L49.3275 17H47.8072L43.4507 3.34229L39.0715 17H37.5739Z" fill="#92E7E4" />
@@ -26,7 +29,7 @@ export default function Home() {
 Где купить
       </a></button>
     </div>
-
+  </div>
     </div>
 
 
@@ -145,8 +148,8 @@ export default function Home() {
     </div>
   </div>
 </div>
-
-   <Footer /> */}
+*/}
+   <Footer /> 
     </div>
   
   );
