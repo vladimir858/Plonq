db2885abde4a5a9e720ed04fdbbee20be2a5b5c7
vladimir858 <standardwebsites@gmail.com>
2026-02-06 16:08:18 +0300

    fix product

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css' 
+import './global.css' 
 import './all.css' 
 import './reset.css' 
 
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -19,27 +19,38 @@ export default function Product() {
 <img src="plonqX-two.png" />
         </div>
     </main>
- <main className='main'>
-    <div className="main-wrap-img">
+ <main className='main-two'>
+    <div className="main-wrap-img-two">
 <img src="capsule.png" />
         </div>
   <div className="main-wrap-title">
-    <div className="main-headtitle">Устройство Plonq X</div>
-        <div className="main-title">Устройство Plonq X — это электронный испаритель, который преобразует жидкость в пар.</div>
-        <div className="main-subtitle">Plonq оснащен вибрацией, круговым
-           светодиодом и аккумулятором 360 mAh, которого хватает 
-           на целый день умеренного использования в паре со смартфоном. 
+    <div className="main-headtitle">Капсула Plonq POD </div>
+        <div className="main-title">Plonq POD - это одноразовая капсула с жидкостью. Жидкость создана и заправлена при строгом контроле.</div>
+        <div className="main-subtitle">Капсулы не предназначены 
+          для повторной заправки. 1 капсула в среднем рассчитана на 300 затяжек, 
+          что приблизительно равно 1,5-2 пачек сигарет.
             </div>
-        <div className="main-subtitle">
- Благодаря порту USB C устройство заряжается менее чем за час.
-        </div>
 </div>
-
       
  </main>
-
-
 </div>
+ <main className="main-gray">
+  <div className="container">
+<div className="main-gray-wrap">
+
+
+  <div className="main-wrap-title">
+    <div className="main-headtitle">Устройство Plonq 500 </div>
+        <div className="main-title">Одноразовый электронный испаритель, который преобразует жидкость в пар.</div>
+        <div className="main-subtitle">Plonq 500 рассчитан на 500 затяжек и не предназначен для повторной перезаправки и подзарядки.
+            </div>
+</div>
+        <div className="main-wrap-img-tree">
+<img src="vape-plonq.png" />
+        </div> 
+  </div>
+  </div>
+ </main>
   </div>
   );
 }
